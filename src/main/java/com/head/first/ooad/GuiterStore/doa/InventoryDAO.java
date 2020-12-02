107c63f405772f8199891ea0ced72a28edff671a
Arx1971 <adnan.rahin13@gmail.com>
2020-12-02 01:41:52 -0500

    Local Changes are Made

--- src/main/java/com/head/first/ooad/GuiterStore/doa/InventoryDAO.java
+++ src/main/java/com/head/first/ooad/GuiterStore/doa/InventoryDAO.java
@@ -1,4 +1,23 @@
 package com.head.first.ooad.GuiterStore.doa;
 
+import com.head.first.ooad.GuiterStore.domain.Guitar;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InventoryDAO {
+
+    private List<Guitar> guitars = new ArrayList<>();
+
+    private InventoryDAO(List<Guitar> guitars) {
+        this.guitars = guitars;
+    }
+
+    public List<Guitar> getGuitars() {
+        return guitars;
+    }
+
+    public void setGuitars(List<Guitar> guitars) {
+        this.guitars = guitars;
+    }
 }
